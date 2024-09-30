2eb6021cd503d8a4c5cd26df199c5bb3c27f894c
sctlcd <lucinda.souchet@gmail.com>
2024-10-01 00:32:41 +0200

    added alert message by passing message prop to Alert component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <div className='container mx-auto'>
         <Alert 
           type={"info"}
+          message= {"sorry not sorry! jaja"}
         />
       </div>
     </div>
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,10 +1,18 @@
 import React, {useState, useEffect} from 'react';
+// import Alert from './Alert';
 
-export default function Alert({type}) {
+export default function Alert({type, message}) {
   const [showAlert, setShowAlert] = useState(true);
   return (
     showAlert && (
-    <div className={`alert alert-${type}`}><div>{type} alert</div></div>
+    <div className={`alert alert-${type}`}>
+      <div>
+        {type} alert
+        <div className='alert-close'>
+          <span className='mr-1'>{message}</span>
+        </div>
+      </div>
+    </div>
     )
   );
 }
